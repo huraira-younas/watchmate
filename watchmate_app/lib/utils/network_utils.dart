e6d6ec525ecba1b582f4f44bef00e033e90873c7
Senpai <raohuraira331.rb@gmail.com>
2025-07-30 02:13:10 +0500

    Added

--- watchmate_app/lib/utils/network_utils.dart
+++ watchmate_app/lib/utils/network_utils.dart
@@ -4,6 +4,6 @@ class NetworkUtils {
   static const _defaultLocalUrl = 'http://192.168.1.6:$_defaultPort';
   static const _defaultPort = 5000;
 
-  static String get prodUrl => "https://api.yourdomain.com";
+  static String get prodUrl => "https://4df5fed0-26d0-4913-980b-18bc46d9b32d-00-1fp1uf95mtg0y.pike.replit.dev";
   static String get baseUrl => kDebugMode ? _defaultLocalUrl : prodUrl;
 }
