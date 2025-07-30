6129617693fc690becac50f08b7fc2b2a06ca967
Senpai <raohuraira331.rb@gmail.com>
2025-07-30 22:18:05 +0500

    Added Routes

--- watchmate_app/lib/router/app_routes.dart
+++ watchmate_app/lib/router/app_routes.dart
@@ -4,12 +4,16 @@ import 'package:go_router/go_router.dart'
 import 'package:watchmate_app/router/routes/exports.dart';
 import 'package:flutter/material.dart' show BuildContext;
 import 'package:watchmate_app/layouts/home_layout.dart';
+import 'package:watchmate_app/services/shared_prefs.dart';
 
 abstract class AppRoutes {
   static final layout = LayoutRoutes.all;
   static final auth = AuthRoutes.all;
 
   static String? handle(BuildContext cyx, GoRouterState state) {
+    final user = SharedPrefs.instance.getLoggedUser();
+    if (user == null) return AuthRoutes.login.path;
+
     return null;
   }
 
