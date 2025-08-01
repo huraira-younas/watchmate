94be8a49072d8f6a2acd8c41c412f944f3baa150
Senpai <raohuraira331.rb@gmail.com>
2025-08-02 00:38:56 +0500

    Fixed bugs

--- watchmate_app/lib/features/home/views/home_screen.dart
+++ watchmate_app/lib/features/home/views/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:watchmate_app/common/widgets/hls_player.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     return Scaffold(
       body: Center(child: HlsVideoPlayer(folder: "test_video")),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
