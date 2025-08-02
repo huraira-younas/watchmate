f58969d4a3149306634f6465522a441bffcb06f4
Senpai <raohuraira331.rb@gmail.com>
2025-08-02 23:42:38 +0500

    Added Tabs

--- watchmate_app/lib/extensions/widget_extensions.dart
+++ watchmate_app/lib/extensions/widget_extensions.dart
@@ -24,4 +24,12 @@ extension WidgetX on Widget {
 
   Widget expanded({int flex = 1, Key? key}) =>
       Expanded(flex: flex, key: key, child: this);
+
+  Widget center({Key? key, double? widthFactor, double? heightFactor}) =>
+      Center(
+        heightFactor: heightFactor,
+        widthFactor: widthFactor,
+        key: key,
+        child: this,
+      );
 }
