6035e0702483ea6a072cfe0696d0b992ff42e8b2
Senpai <raohuraira331.rb@gmail.com>
2025-08-07 17:57:38 +0500

    Fixed bugs

--- watchmate_app/lib/features/my_list/views/my_list_screen.dart
+++ watchmate_app/lib/features/my_list/views/my_list_screen.dart
@@ -27,7 +27,11 @@ class _MyListScreenState extends State<MyListScreen>
   @override
   void initState() {
     super.initState();
-    _vidCubit.getAllVideos(
+    _refresh();
+  }
+
+  Future<void> _refresh() async {
+    await _vidCubit.getAllVideos(
       userId: _authBloc.user!.id,
       visibility: _key,
       refresh: true,
@@ -38,46 +42,49 @@ class _MyListScreenState extends State<MyListScreen>
   Widget build(BuildContext context) {
     super.build(context);
 
-    return BlocBuilder<VideoCubit, Map<String, VideoState>>(
-      buildWhen: (p, c) => c[_key] != p[_key],
-      builder: (context, state) {
-        final st = state[_key];
-        if (st == null) return const SizedBox.shrink();
+    return RefreshIndicator(
+      onRefresh: _refresh,
+      child: BlocBuilder<VideoCubit, Map<String, VideoState>>(
+        buildWhen: (p, c) => c[_key] != p[_key],
+        builder: (context, state) {
+          final st = state[_key];
+          if (st == null) return const SizedBox.shrink();
 
-        final loading = st.loading;
-        final error = st.error;
+          final loading = st.loading;
+          final error = st.error;
 
-        if (error != null) {
-          return CustomLabelWidget(
-            icon: Icons.streetview_sharp,
-            text: error.message,
-            title: error.title,
-          ).fadeIn();
-        }
+          if (error != null) {
+            return CustomLabelWidget(
+              icon: Icons.streetview_sharp,
+              text: error.message,
+              title: error.title,
+            ).fadeIn();
+          }
 
-        if (loading != null) return const VideoCardSkeleton().fadeIn();
+          if (loading != null) return const VideoCardSkeleton().fadeIn();
 
-        final pagination = st.pagination;
-        if (pagination.videos.isEmpty) {
-          return const CustomLabelWidget(
-            text: "No videos found in your list. Try to add one",
-            icon: Icons.insert_emoticon_sharp,
-            title: "Oppss.. No Video Found",
+          final pagination = st.pagination;
+          if (pagination.videos.isEmpty) {
+            return const CustomLabelWidget(
+              text: "No videos found in your list. Try to add one",
+              icon: Icons.insert_emoticon_sharp,
+              title: "Oppss.. No Video Found",
+            );
+          }
+
+          return ListView.separated(
+            itemCount: pagination.videos.length,
+            padding: const EdgeInsets.symmetric(
+              horizontal: AppConstants.padding - 12,
+              vertical: AppConstants.padding - 10,
+            ),
+            separatorBuilder: (_, _) => const SizedBox(height: 10),
+            itemBuilder: (context, idx) {
+              return VideoPreview(video: pagination.videos[idx]);
+            },
           );
-        }
-
-        return ListView.separated(
-          itemCount: pagination.videos.length,
-          padding: const EdgeInsets.symmetric(
-            horizontal: AppConstants.padding - 12,
-            vertical: AppConstants.padding - 10,
-          ),
-          separatorBuilder: (_, _) => const SizedBox(height: 10),
-          itemBuilder: (context, idx) {
-            return VideoPreview(video: pagination.videos[idx]);
-          },
-        );
-      },
+        },
+      ),
     );
   }
 
