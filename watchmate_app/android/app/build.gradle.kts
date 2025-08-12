a285f17facbc2f175c97d2ff7202430706bbd0ae
Senpai <raohuraira331.rb@gmail.com>
2025-08-12 21:35:27 +0500

    Fixed bugs

--- watchmate_app/android/app/build.gradle.kts
+++ watchmate_app/android/app/build.gradle.kts
@@ -1,8 +1,17 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -21,15 +30,24 @@ android {
 
     defaultConfig {
         applicationId = "senpai.watchmate_app"
-        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdk = flutter.minSdkVersion
+    }
+
+    signingConfigs {
+        create("release") {
+            storePassword = keystoreProperties.getProperty("storePassword")
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+        }
     }
 
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
